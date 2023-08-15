45aaa4615a42f9768bb6d5752f527811c0537ba6
Guilherme C <62406808+guilherme-couto@users.noreply.github.com>
2023-08-15 15:48:01 -0300

    New configuration of files for bigger simulations

--- execution.py
+++ execution.py
@@ -1,4 +1,11 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import imageio.v2
+import math
+
 # Possible parameters
+dxs = ['0.020']
 cell_models = ['AFHN', 'AFHN-Fibro']
 numbers_threads = [4]
 dts_ODE = [0.02, 0.04, 0.06, 0.08, 0.1, 0.2]
--- script.py
+++ script.py
@@ -1,24 +1,23 @@
-import os
-
 from execution import *
 
-for cell_model in cell_models:
-    for num_threads in numbers_threads:
-        for method in methods:
-            for dt_ODE in dts_ODE:
-                dts_PDE = [dt_ODE]
-                if method == 'ADI1' or method == 'FE':
-                    i = 2
-                    while dt_ODE * i <= max_dt_PDE:
-                        dts_PDE.append(dt_ODE * i)
-                        i += 1
-                for dt_PDE in dts_PDE:
-                    execution_line = ''
-                    if cell_model.find('Fibro') != -1:
-                        execution_line = f'./main {num_threads} {dt_ODE} {dt_PDE} {method} 1'
-                    else:
-                        execution_line = f'./main {num_threads} {dt_ODE} {dt_PDE} {method} 0'
-                    print(f'Executing {execution_line}\n')
-                    os.system(f'{execution_line}')
-                    print(f'Simultation {execution_line} finished!\n')
+for dx in dxs:
+    for cell_model in cell_models:
+        for num_threads in numbers_threads:
+            for method in methods:
+                for dt_ODE in dts_ODE:
+                    dts_PDE = [dt_ODE]
+                    if method == 'ADI1' or method == 'FE':
+                        i = 2
+                        while dt_ODE * i <= max_dt_PDE:
+                            dts_PDE.append(dt_ODE * i)
+                            i += 1
+                    for dt_PDE in dts_PDE:
+                        execution_line = ''
+                        if cell_model.find('Fibro') != -1:
+                            execution_line = f'./main {num_threads} {dt_ODE} {dt_PDE} {method} 1'
+                        else:
+                            execution_line = f'./main {num_threads} {dt_ODE} {dt_PDE} {method} 0'
+                        print(f'Executing {execution_line}\n')
+                        os.system(f'{execution_line}')
+                        print(f'Simultation {execution_line} finished!\n')
                     
